95225654bfd1b82bc8cf0e4952b0edb45830f337
Conner Tenn <connertenn@gmail.com>
2025-01-14 23:33:42 -0500

    Added basic GPIO definitions

--- pico.zig
+++ pico.zig
@@ -2,3 +2,11 @@ const std = @import("std");
 
 pub const csdk = @import("csdk.zig").csdk;
 pub const stdio = @import("stdio.zig");
+
+pub const GPIO_IN = false;
+pub const GPIO_OUT = true;
+
+pub const GPIO_HIGH = true;
+pub const GPIO_LOW = false;
+
+pub const LED_PIN = csdk.PICO_DEFAULT_LED_PIN;
