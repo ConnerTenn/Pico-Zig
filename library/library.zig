277f3bd24e136918b512ebc139c3633472f7988d
Conner Tenn <connertenn@gmail.com>
2025-02-02 23:27:00 -0500

    Added support to measure duty cycle

--- library/library.zig
+++ library/library.zig
@@ -2,3 +2,4 @@ pub const foc = @import("foc.zig");
 pub const motor = @import("motor.zig");
 pub const pid = @import("pid.zig");
 pub const LIS3MDL = @import("LIS3MDL.zig");
+pub const duty_cycle = @import("duty_cycle.zig");
