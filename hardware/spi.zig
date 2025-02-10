6abeecaaa8c9bebdd4196d184f2663322fa7e8b8
Conner Tenn <connertenn@gmail.com>
2025-02-10 00:07:40 -0500

    Fixed issues with the gu128x32 driver
    Updated the SPI driver

--- hardware/spi.zig
+++ hardware/spi.zig
@@ -15,13 +15,33 @@ pub const SPI = struct {
     cs_pin: gpio.Pin,
     hardware_spi: *csdk.spi_inst_t,
     baudrate_hz: u32,
+    clock_polarity: ClockPolarity,
+    clock_phase: ClockPhase,
 
     pub const SpiHw = enum {
         spi0,
         spi1,
     };
 
-    pub fn create(sck_pin: gpio.Pin, tx_pin: gpio.Pin, rx_pin: gpio.Pin, cs_pin: gpio.Pin, spi_hw: SpiHw, baudrate_hz: u32) Self {
+    pub const ClockPolarity = enum {
+        idle_low,
+        idle_high,
+    };
+    pub const ClockPhase = enum {
+        first_edge,
+        second_edge,
+    };
+
+    pub fn create(
+        sck_pin: gpio.Pin,
+        tx_pin: gpio.Pin,
+        rx_pin: gpio.Pin,
+        cs_pin: gpio.Pin,
+        spi_hw: SpiHw,
+        baudrate_hz: u32,
+        clock_polarity: ClockPolarity,
+        clock_phase: ClockPhase,
+    ) Self {
         const hardware_spi = switch (spi_hw) {
             .spi0 => csdk.spi0_hw,
             .spi1 => csdk.spi1_hw,
@@ -34,16 +54,33 @@ pub const SPI = struct {
             .cs_pin = cs_pin,
             .hardware_spi = @ptrCast(hardware_spi),
             .baudrate_hz = baudrate_hz,
+            .clock_polarity = clock_polarity,
+            .clock_phase = clock_phase,
         };
     }
 
     pub fn init(self: *Self) void {
+        self.csDeselect();
         self.cs_pin.init(gpio.Pin.Config{
             .direction = .out,
         });
-        self.cs_pin.put(false);
 
         self.baudrate_hz = csdk.spi_init(self.hardware_spi, self.baudrate_hz);
+
+        csdk.spi_set_format(
+            self.hardware_spi,
+            8,
+            switch (self.clock_polarity) {
+                .idle_low => csdk.SPI_CPOL_0,
+                .idle_high => csdk.SPI_CPOL_1,
+            },
+            switch (self.clock_phase) {
+                .first_edge => csdk.SPI_CPHA_0,
+                .second_edge => csdk.SPI_CPHA_1,
+            },
+            csdk.SPI_MSB_FIRST,
+        );
+
         stdio.print("SPI baudrate:{}\n", .{self.baudrate_hz});
         csdk.gpio_set_function(self.sck_pin.toSdkPin(), csdk.GPIO_FUNC_SPI);
         csdk.gpio_set_function(self.tx_pin.toSdkPin(), csdk.GPIO_FUNC_SPI);
@@ -58,24 +95,32 @@ pub const SPI = struct {
         self.cs_pin.put(true);
     }
 
-    pub fn writeRead(self: Self, len: comptime_int, write_data: [len]u8) [len]u8 {
-        self.csSelect();
+    pub fn writeRead(self: Self, len: comptime_int, write_data: [len]u8, comptime drive_chip_select: bool) [len]u8 {
+        if (drive_chip_select) {
+            self.csSelect();
+        }
 
         var read_data: [len]u8 = .{0} ** len;
         _ = csdk.spi_write_read_blocking(self.hardware_spi, &write_data, &read_data, len);
 
-        self.csDeselect();
-        csdk.sleep_us(10);
+        if (drive_chip_select) {
+            self.csDeselect();
+        }
+        // csdk.sleep_us(10);
 
         return read_data;
     }
 
-    pub fn write(self: Self, len: comptime_int, write_data: [len]u8) void {
-        self.csSelect();
+    pub fn write(self: Self, len: comptime_int, write_data: [len]u8, comptime drive_chip_select: bool) void {
+        if (drive_chip_select) {
+            self.csSelect();
+        }
 
         _ = csdk.spi_write_blocking(self.hardware_spi, &write_data, len);
 
-        self.csDeselect();
-        csdk.sleep_us(10);
+        if (drive_chip_select) {
+            self.csDeselect();
+        }
+        // csdk.sleep_us(10);
     }
 };
--- library/gu128x32.zig
+++ library/gu128x32.zig
@@ -14,11 +14,28 @@ pub const GU128x32 = struct {
     cmd_data_pin: gpio.Pin,
     frame_pulse_pin: gpio.Pin,
 
-    pub fn create(sck_pin: gpio.Pin, tx_pin: gpio.Pin, rx_pin: gpio.Pin, cs_pin: gpio.Pin, cmd_data_pin: gpio.Pin, frame_pulse_pin: gpio.Pin, spi_hw: spi.SPI.SpiHw) Self {
-        const min_cycle_time_ns = 80 * 2;
+    pub fn create(
+        sck_pin: gpio.Pin,
+        tx_pin: gpio.Pin,
+        rx_pin: gpio.Pin,
+        cs_pin: gpio.Pin,
+        cmd_data_pin: gpio.Pin,
+        frame_pulse_pin: gpio.Pin,
+        spi_hw: spi.SPI.SpiHw,
+    ) Self {
+        const min_cycle_time_ns = 80 * 2; //80 * 2;
         const baudrate_hz = math.pow(u32, 10, 9) / min_cycle_time_ns;
         return Self{
-            .spi = spi.SPI.create(sck_pin, tx_pin, rx_pin, cs_pin, spi_hw, baudrate_hz),
+            .spi = spi.SPI.create(
+                sck_pin,
+                tx_pin,
+                rx_pin,
+                cs_pin,
+                spi_hw,
+                baudrate_hz,
+                .idle_high,
+                .second_edge,
+            ),
             .cmd_data_pin = cmd_data_pin,
             .frame_pulse_pin = frame_pulse_pin,
         };
@@ -29,6 +46,42 @@ pub const GU128x32 = struct {
         self.cmd_data_pin.init(.{
             .direction = .out,
         });
+
+        // == Display initialization sequence ==
+        // Clear the display
+        self.writeCommand(DisplayClear{
+            .byte1 = .{
+                .reset_addresses = 1,
+                .gram_area0_clear = 1,
+                .gram_area1_clear = 1,
+            },
+        });
+        csdk.sleep_ms(2);
+
+        // Set all regions to GRAM
+        for (0..8) |idx| {
+            self.writeCommand(DisplayAreaSet{
+                .byte1 = .{},
+                .byte2 = .{
+                    .area = @intCast(idx),
+                },
+                .byte3 = .{},
+            });
+        }
+
+        // Set up the initial X and Y addresses
+        self.writeCommand(pico.library.gu128x32.GU128x32.DataWriteXAddress{
+            .byte1 = .{},
+            .byte2 = .{
+                .gram_x_addr = 0,
+            },
+        });
+        self.writeCommand(pico.library.gu128x32.GU128x32.DataWriteYAddress{
+            .byte1 = .{},
+            .byte2 = .{
+                .gram_y_addr = 0,
+            },
+        });
     }
 
     const WriteType = enum(u1) {
@@ -37,8 +90,9 @@ pub const GU128x32 = struct {
     };
 
     pub fn write(self: *Self, write_type: WriteType, data: u8) void {
-        self.cmd_data_pin.put(@intFromEnum(write_type) == 1);
-        self.spi.write(1, [_]u8{data});
+        self.cmd_data_pin.put(write_type == .command);
+        // pico.stdio.print("transmit: {X:0>2}\n", .{data});
+        self.spi.write(1, [_]u8{data}, true);
     }
 
     pub const DisplayOnOff = struct {
@@ -76,7 +130,7 @@ pub const GU128x32 = struct {
         },
     };
 
-    const BrightnessSet = struct {
+    pub const BrightnessSet = struct {
         byte1: packed struct {
             const write_type: WriteType = .command;
 
@@ -85,19 +139,19 @@ pub const GU128x32 = struct {
         },
     };
 
-    const DisplayClear = struct {
+    pub const DisplayClear = struct {
         byte1: packed struct {
             const write_type: WriteType = .command;
 
-            hm: u1,
+            reset_addresses: u1,
             reserved_0: u1 = 1,
-            g0c: u1,
-            g1c: u1,
+            gram_area0_clear: u1,
+            gram_area1_clear: u1,
             reserved_1: u4 = 0b0101,
         },
     };
 
-    const DisplayAreaSet = struct {
+    pub const DisplayAreaSet = struct {
         byte1: packed struct {
             const write_type: WriteType = .command;
 
@@ -118,7 +172,7 @@ pub const GU128x32 = struct {
         },
     };
 
-    const DataWriteXAddress = struct {
+    pub const DataWriteXAddress = struct {
         byte1: packed struct {
             const write_type: WriteType = .command;
 
@@ -128,11 +182,12 @@ pub const GU128x32 = struct {
         byte2: packed struct {
             const write_type: WriteType = .command;
 
-            gram_x_addr: u8,
+            gram_x_addr: u7,
+            reserved_0: u1 = 0,
         },
     };
 
-    const DataWriteYAddress = struct {
+    pub const DataWriteYAddress = struct {
         byte1: packed struct {
             const write_type: WriteType = .command;
 
@@ -147,7 +202,7 @@ pub const GU128x32 = struct {
         },
     };
 
-    const DataStartXAddress = struct {
+    pub const DataStartXAddress = struct {
         byte1: packed struct {
             const write_type: WriteType = .command;
 
@@ -161,30 +216,61 @@ pub const GU128x32 = struct {
         },
     };
 
-    const DataStartYAddress = struct {
+    pub const DataStartYAddress = struct {
         byte1: packed struct {
             const write_type: WriteType = .command;
 
             reserved_0: u1 = 0,
-            s0: u1,
-            s1: u1,
-            ud: u1,
+            shift: enum(u2) {
+                shift_none = 0b00,
+                shift_8_dots = 0b01,
+                shift_1_dots = 0b10,
+                shift_2_dots = 0b11,
+            },
+            ud: enum(u1) {
+                scrolled_up = 1,
+                scrolled_down = 0,
+            },
             reserved_1: u4 = 0b1011,
         },
     };
 
-    const AddressModeSet = struct {
+    pub const AddressModeSet = struct {
         byte1: packed struct {
             const write_type: WriteType = .command;
 
             reserved_0: u1 = 0,
-            igy: u1,
-            igx: u1,
+            increment_y: enum(u1) {
+                fixed = 0b0,
+                increment = 0b1,
+            },
+            increment_x: enum(u1) {
+                fixed = 0b0,
+                increment = 0b1,
+            },
             reserved_1: u5 = 0b10000,
         },
     };
 
-    const DataWrite = struct {
+    pub const AddressRead = struct {
+        byte1: packed struct {
+            const write_type: WriteType = .command;
+
+            reserved_0: u8 = 0b11010100,
+        } = .{},
+        byte2: packed struct {
+            const write_type: WriteType = .command;
+
+            reserved_0: u8 = 0b00000000,
+        } = .{},
+        byte3: packed struct {
+            const write_type: WriteType = .command;
+
+            reserved_0: u8 = 0b00000000,
+        } = .{},
+    };
+
+    pub const DataWrite = struct {
         byte1: packed struct {
             const write_type: WriteType = .data;
 
@@ -195,7 +281,7 @@ pub const GU128x32 = struct {
     pub fn writeCommand(self: *Self, cmd: anytype) void {
         const info = @typeInfo(@TypeOf(cmd));
 
-        //Loop through each byte in the command
+        //Loop through each byte field in the command
         const fields = info.Struct.fields;
         inline for (fields) |field| {
             //Get the command byte
