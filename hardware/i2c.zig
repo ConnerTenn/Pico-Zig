ff64c0ccb4f7705a7f283e74da0c6a01605ab749
Conner Tenn <connertenn@gmail.com>
2025-05-01 01:11:55 -0400

    Added support for mpu6050 IMU data reading

--- hardware/i2c.zig
+++ hardware/i2c.zig
@@ -9,7 +9,7 @@ const Pin = hardware.gpio.Pin;
 pub const I2C = struct {
     const Self = @This();
 
-    i2c_instance: csdk.i2c_inst_t,
+    i2c_instance: *csdk.i2c_inst_t,
     baudrate: u32,
     sda_pin: Pin,
     scl_pin: Pin,
@@ -21,8 +21,8 @@ pub const I2C = struct {
 
     pub fn create(instance: Instance, baudrate: u32, sda_pin: Pin, scl_pin: Pin) Self {
         const i2c_instance = switch (instance) {
-            .i2c0 => csdk.i2c0_inst,
-            .i2c1 => csdk.i2c1_inst,
+            .i2c0 => &csdk.i2c0_inst,
+            .i2c1 => &csdk.i2c1_inst,
         };
 
         return Self{
@@ -34,11 +34,32 @@ pub const I2C = struct {
     }
 
     pub fn init(self: *Self) void {
-        csdk.i2c_init(self.i2c_instance, self.baudrate);
+        _ = csdk.i2c_init(self.i2c_instance, self.baudrate);
 
         csdk.gpio_set_function(csdk.PICO_DEFAULT_I2C_SDA_PIN, csdk.GPIO_FUNC_I2C);
         csdk.gpio_set_function(csdk.PICO_DEFAULT_I2C_SCL_PIN, csdk.GPIO_FUNC_I2C);
         csdk.gpio_pull_up(self.sda_pin.toSdkPin());
         csdk.gpio_pull_up(self.scl_pin.toSdkPin());
+    }
+
+    const StopCondition = enum {
+        nostop,
+        stop,
+    };
+
+    pub fn readBlocking(self: *Self, i2c_addr: u7, data: *anyopaque, size: usize, stop_condition: StopCondition) void {
+        const nostop = switch (stop_condition) {
+            .nostop => true,
+            .stop => false,
+        };
+        _ = csdk.i2c_read_blocking(self.i2c_instance, @intCast(i2c_addr), @ptrCast(data), size, nostop);
+    }
+
+    pub fn writeBlocking(self: *Self, i2c_addr: u7, data: *const anyopaque, size: usize, stop_condition: StopCondition) void {
+        const nostop = switch (stop_condition) {
+            .nostop => true,
+            .stop => false,
+        };
+        _ = csdk.i2c_write_blocking(self.i2c_instance, @intCast(i2c_addr), @ptrCast(data), size, nostop);
     }
 };
--- library/mpu6050.zig
+++ library/mpu6050.zig
@@ -11,6 +11,7 @@ pub const MPU6050 = struct {
     const Self = @This();
 
     pub const baudrate = 400 * 1000;
+    pub const i2c_addr = 0x68;
 
     i2c: hardware.i2c.I2C,
 
@@ -19,4 +20,112 @@ pub const MPU6050 = struct {
             .i2c = i2c,
         };
     }
+
+    pub fn init(self: *Self) void {
+        self.i2c.init();
+
+        self.reset();
+    }
+
+    pub fn reset(self: *Self) void {
+        self.writeReg(PowerManagement1{
+            .clk_sel = 0,
+            .temp_disable = 0,
+            .cycle = 0,
+            .sleep = 0,
+            .device_reset = 1,
+        });
+        csdk.sleep_ms(100); // Allow device to reset and stabilize
+
+        self.writeReg(PowerManagement1{
+            .clk_sel = 0,
+            .temp_disable = 0,
+            .cycle = 0,
+            .sleep = 0,
+            .device_reset = 0,
+        });
+        csdk.sleep_ms(10); // Allow stabilization after waking up
+    }
+
+    const RawImuData = struct {
+        const address = 0x3B;
+
+        accel_x: i16,
+        accel_y: i16,
+        accel_z: i16,
+        gyro_x: i16,
+        gyro_y: i16,
+        gyro_z: i16,
+
+        fn create(accel_data: AccelerometerRegisters, gyro_data: GyroscopeRegisters) RawImuData {
+            return RawImuData{
+                .accel_x = (@as(i16, @intCast(accel_data.accel_x_high)) << 8) | (accel_data.accel_x_low),
+                .accel_y = (@as(i16, @intCast(accel_data.accel_y_high)) << 8) | (accel_data.accel_y_low),
+                .accel_z = (@as(i16, @intCast(accel_data.accel_z_high)) << 8) | (accel_data.accel_z_low),
+
+                .gyro_x = (@as(i16, @intCast(gyro_data.gyro_x_high)) << 8) | (gyro_data.gyro_x_low),
+                .gyro_y = (@as(i16, @intCast(gyro_data.gyro_y_high)) << 8) | (gyro_data.gyro_y_low),
+                .gyro_z = (@as(i16, @intCast(gyro_data.gyro_z_high)) << 8) | (gyro_data.gyro_z_low),
+            };
+        }
+    };
+
+    pub fn getRawImuData(self: *Self) RawImuData {
+        const accel_data = self.readReg(AccelerometerRegisters);
+        const gyro_data = self.readReg(GyroscopeRegisters);
+
+        return RawImuData.create(accel_data, gyro_data);
+    }
+
+    pub fn readReg(self: *Self, Reg: type) Reg {
+        const addr: usize = Reg.address;
+        self.i2c.writeBlocking(i2c_addr, @ptrCast(&addr), 1, .nostop);
+
+        var reg: Reg = undefined;
+        self.i2c.readBlocking(i2c_addr, @ptrCast(&reg), @sizeOf(Reg), .stop);
+        return reg;
+    }
+
+    pub fn writeReg(self: *Self, reg: anytype) void {
+        const Reg = @TypeOf(reg);
+        const addr: usize = Reg.address;
+        self.i2c.writeBlocking(i2c_addr, @ptrCast(&addr), 1, .nostop);
+        self.i2c.writeBlocking(i2c_addr, @ptrCast(&reg), @sizeOf(Reg), .stop);
+    }
+
+    const AccelerometerRegisters = packed struct {
+        const address = 0x3B;
+
+        accel_x_high: i8,
+        accel_x_low: i8,
+        accel_y_high: i8,
+        accel_y_low: i8,
+        accel_z_high: i8,
+        accel_z_low: i8,
+        gyro_x: i16,
+        gyro_y: i16,
+        gyro_z: i16,
+    };
+
+    const GyroscopeRegisters = packed struct {
+        const address = 0x43;
+
+        gyro_x_high: i8,
+        gyro_x_low: i8,
+        gyro_y_high: i8,
+        gyro_y_low: i8,
+        gyro_z_high: i8,
+        gyro_z_low: i8,
+    };
+
+    const PowerManagement1 = packed struct {
+        const address = 0x68;
+
+        clk_sel: u3,
+        temp_disable: u1,
+        _: u1 = 0,
+        cycle: u1,
+        sleep: u1,
+        device_reset: u1,
+    };
 };
