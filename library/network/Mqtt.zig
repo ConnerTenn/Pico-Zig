ed1e0506308d960e3e24d4c1fd0ad04070ad8dcb
Conner Tenn <connertenn@gmail.com>
2025-08-31 18:39:31 -0400

    Improved the callback handling
    Added support for large messages

--- library/network/Mqtt.zig
+++ library/network/Mqtt.zig
@@ -8,6 +8,13 @@ const global_allocator = pico.library.alloc.global_allocator;
 
 const Mqtt = @This();
 
+mqtt_client: *csdk.mqtt_client_t,
+
+active_topic: ?Topic,
+active_message: ?MessageBuffer,
+
+callbacks: ?Callbacks,
+
 const QOS = enum(u8) {
     AtMostOnce = 0,
     AtLeastOnce = 1,
@@ -19,18 +26,65 @@ const Retain = enum(u8) {
     true = 1,
 };
 
-pub const RecvMessageCallback = struct {
+pub const Callbacks = struct {
     ctx: ?*anyopaque,
-    callback: *const fn (ctx: ?*anyopaque, topic: [:0]const u8, message: []const u8) void,
+    connected_callback: *const fn (ctx: ?*anyopaque) void,
+    message_recv_callback: *const fn (ctx: ?*anyopaque, topic: [:0]const u8, message: []const u8) void,
 };
-// pub const RecvMessageCallback
 
-pub const CallbackHeap = std.StringHashMap(RecvMessageCallback);
+pub const Topic = struct {
+    topic: [:0]const u8,
 
-mqtt_client: *csdk.mqtt_client_t,
-topic: ?[:0]u8,
+    pub fn new(topic: [:0]const u8) Topic {
+        return Topic{
+            .topic = topic,
+        };
+    }
 
-callbacks: CallbackHeap,
+    pub fn initAlloc(topic: [*:0]const u8) !Topic {
+        const topic_len = std.mem.len(topic);
+        const topic_buffer = try global_allocator.alloc(u8, topic_len + 1);
+
+        @memcpy(topic_buffer, topic);
+        topic_buffer[topic_len] = 0; //Ensure sentinal termination
+
+        return Topic.new(topic_buffer[0..topic_len :0]);
+    }
+
+    pub fn deinit(self: *Topic) void {
+        global_allocator.free(self.topic);
+    }
+
+    pub fn length(self: *const Topic) usize {
+        return std.mem.len(self.topic);
+    }
+
+    pub fn getSlice(self: *Topic) [:0]const u8 {
+        return self.topic;
+    }
+};
+
+pub const MessageBuffer = struct {
+    buffer: std.ArrayList(u8),
+
+    pub fn init(len: usize) !MessageBuffer {
+        return MessageBuffer{
+            .buffer = try std.ArrayList(u8).initCapacity(global_allocator, len),
+        };
+    }
+
+    pub fn deinit(self: *MessageBuffer) void {
+        self.buffer.deinit();
+    }
+
+    pub fn appendSlice(self: *MessageBuffer, items: []const u8) !void {
+        try self.buffer.appendSlice(items);
+    }
+
+    pub fn getSlice(self: *MessageBuffer) []const u8 {
+        return self.buffer.items;
+    }
+};
 
 pub fn new() !Mqtt {
     const mqtt_client = csdk.mqtt_client_new() orelse {
@@ -39,8 +93,9 @@ pub fn new() !Mqtt {
 
     return Mqtt{
         .mqtt_client = mqtt_client,
-        .topic = null,
-        .callbacks = CallbackHeap.init(global_allocator),
+        .active_topic = null,
+        .active_message = null,
+        .callbacks = null,
     };
 }
 
@@ -51,12 +106,16 @@ pub fn destroy(self: *Mqtt) void {
     }
 }
 
-pub fn connect(self: *Mqtt, address: network.IpV4Addr, port: u16) !void {
+pub fn setCallbacks(self: *Mqtt, callbacks: Callbacks) void {
+    self.callbacks = callbacks;
+}
+
+pub fn connect(self: *Mqtt, address: network.IpV4Addr, port: u16, id_topic: [:0]const u8) !void {
     network.enterCriticalSection();
     defer network.exitCriticalSection();
 
     const client_info = csdk.mqtt_connect_client_info_t{
-        .client_id = "test/pico",
+        .client_id = id_topic,
         // .client_user
         // .client_pass
         .keep_alive = 60, //[sec]
@@ -85,26 +144,13 @@ pub fn connect(self: *Mqtt, address: network.IpV4Addr, port: u16) !void {
 
     stdio.print("mqtt_set_inpub_callback\n", .{});
     csdk.mqtt_set_inpub_callback(self.mqtt_client, publishCallback, dataCallback, self);
-
-    // const online_msg = "pico online";
-
-    // _ = network.hasError(
-    //     csdk.mqtt_publish(self.mqtt_client, "test/pico", online_msg, online_msg.len, @intFromEnum(QOS.AtLeastOnce), @intFromEnum(Retain.true), mqttPubRequestCallback, self),
-    //     "mqtt_publish() failed",
-    // );
 }
 
 pub fn connected(self: *Mqtt) bool {
     return csdk.mqtt_client_is_connected(self.mqtt_client) == 1;
 }
 
-pub fn subscribe(self: *Mqtt, topic: [:0]const u8, qos: QOS, callback: RecvMessageCallback) !void {
-    try self.subscribeTopic(topic, qos);
-
-    try self.callbacks.put(topic, callback);
-}
-
-fn subscribeTopic(self: *Mqtt, topic: [:0]const u8, qos: QOS) !void {
+pub fn subscribe(self: *Mqtt, topic: [:0]const u8, qos: QOS) !void {
     if (network.hasError(
         csdk.mqtt_sub_unsub(self.mqtt_client, topic.ptr, @intFromEnum(qos), subRequestCallback, self, 1),
         "mqtt_sub_unsub() Failed",
@@ -146,19 +192,16 @@ fn mqttConnectionCallback(client: ?*csdk.mqtt_client_t, arg: ?*anyopaque, status
     }
     stdio.print("Connected to mqtt: {?}\n", .{client});
 
-    self.publish("test/pico", "online", .AtLeastOnce, .true) catch |err| {
-        stdio.print("Failed to publish: {}\n", .{err});
-        unreachable;
-    };
-    // const online_msg = "pico online";
-    // _ = network.hasError(
-    //     csdk.mqtt_publish(self.mqtt_client, "test/pico", online_msg, online_msg.len, @intFromEnum(QOS.AtLeastOnce), @intFromEnum(Retain.true), mqttPubRequestCallback, self),
-    //     "mqtt_publish() failed",
-    // );
+    // Call connected callback
+    if (self.callbacks) |callbacks| {
+        callbacks.connected_callback(callbacks.ctx);
+    }
 }
 
-fn mqttPubRequestCallback(_: ?*anyopaque, err: csdk.err_t) callconv(.C) void {
-    stdio.print("mqttPubRequestCallback\n", .{});
+fn mqttPubRequestCallback(arg: ?*anyopaque, err: csdk.err_t) callconv(.C) void {
+    // stdio.print("mqttPubRequestCallback\n", .{});
+    const self: *Mqtt = @alignCast(@ptrCast(arg.?));
+    _ = self;
 
     _ = network.hasError(
         err,
@@ -174,37 +217,65 @@ fn subRequestCallback(arg: ?*anyopaque, err: csdk.err_t) callconv(.C) void {
 }
 
 fn publishCallback(arg: ?*anyopaque, topic: [*c]const u8, total_len: u32) callconv(.C) void {
-    stdio.print("publishCallback\n", .{});
-    stdio.print("  topic: {s}\n", .{topic});
-    stdio.print("  total_len: {}\n", .{total_len});
+    // stdio.print("publishCallback\n", .{});
+    // stdio.print("  topic: {s}\n", .{topic});
+    // stdio.print("  total_len: {}\n", .{total_len});
     const self: *Mqtt = @alignCast(@ptrCast(arg.?));
 
-    const topic_len = std.mem.len(topic);
-
-    const buffer = (global_allocator.alloc(u8, topic_len + 1) catch |err| {
-        stdio.print("Failed to allocate: {?}", .{err});
+    if (self.active_topic != null) {
+        stdio.print("Error: active_topic must be initalized but it already exists", .{});
         unreachable;
-    });
-    @memcpy(buffer, topic);
-    buffer[topic_len] = 0; //Ensure sentinal termination
-    self.topic = buffer[0..topic_len :0];
+    }
 
-    stdio.print("Publish: {s}\n", .{self.topic.?});
+    if (self.active_message != null) {
+        stdio.print("Error: active_message must be initalized but it already exists", .{});
+        unreachable;
+    }
+
+    self.active_topic = Topic.initAlloc(topic) catch |err| {
+        stdio.print("Failed to init active_topic: {?}", .{err});
+        unreachable;
+    };
+
+    self.active_message = MessageBuffer.init(total_len) catch |err| {
+        stdio.print("Failed to init active_message: {?}", .{err});
+        unreachable;
+    };
 }
 
 fn dataCallback(arg: ?*anyopaque, raw_data: [*c]const u8, len: u16, flags: u8) callconv(.C) void {
-    stdio.print("dataCallback\n", .{});
+    // stdio.print("dataCallback\n", .{});
     const self: *Mqtt = @alignCast(@ptrCast(arg.?));
 
     const message: []const u8 = raw_data[0..len];
-    stdio.print("  recv datamessage: \"{s}\"\n", .{message});
-    stdio.print("  flags: {X:02}\n", .{flags});
 
-    if (self.topic) |topic| {
-        if (self.callbacks.get(topic)) |callback| {
-            callback.callback(callback.ctx, topic, message);
+    // stdio.print("  recv datamessage: \"{s}\"\n", .{message});
+    // stdio.print("  flags: {X:02}\n", .{flags});
+
+    if (self.active_message) |*active_message| {
+        active_message.appendSlice(message) catch |err| {
+            stdio.print("Failed to append message: {}\n", .{err});
+        };
+
+        if (flags == csdk.MQTT_DATA_FLAG_LAST) {
+            if (self.active_topic) |*active_topic| {
+                // Call message callback
+                if (self.callbacks) |callbacks| {
+                    callbacks.message_recv_callback(callbacks.ctx, active_topic.getSlice(), active_message.getSlice());
+                }
+
+                // reset active_topic
+                active_topic.deinit();
+                self.active_topic = null;
+            } else {
+                stdio.print("Error: recieved data without topic\n", .{});
+            }
+
+            // reset active_message
+            active_message.deinit();
+            self.active_message = null;
         }
     } else {
-        stdio.print("Error: recieved data without topic\n", .{});
+        stdio.print("Error: recieved data before active_message was initalized\n", .{});
     }
 }
