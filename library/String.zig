9d93b9f50c38e8c443554fe3ceea1d99ebcb0a1a
Conner Tenn <connertenn@gmail.com>
2025-09-23 21:43:54 -0400

    Added comptime handling

--- library/String.zig
+++ library/String.zig
@@ -7,44 +7,63 @@ const allocator = if (builtin.is_test) std.testing.allocator else pico.library.a
 const String = @This();
 pub const Char = u8;
 
-string: ?[:0]Char,
+comptime_created: bool = false,
+string: ?[:0]const Char,
 
 pub fn create(str: []const Char) String {
-    const new_str = allocator.alloc(Char, str.len + 1) catch unreachable;
+    if (@inComptime()) {
+        //In comptime, we can just raw copy the pointer
+        return String{
+            .comptime_created = true,
+            .string = str[0..str.len :0],
+        };
+    } else {
+        // Allocate new memory for the string
+        const new_str = allocator.alloc(Char, str.len + 1) catch unreachable;
 
-    @memcpy(new_str[0..str.len], str);
-    new_str[str.len] = '\x00';
+        @memcpy(new_str[0..str.len], str);
+        new_str[str.len] = '\x00';
 
-    return String{
-        .string = new_str[0..str.len :0],
-    };
+        return String{
+            .string = new_str[0..str.len :0],
+        };
+    }
 }
 
 pub fn destroy(self: *String) void {
     if (self.string) |string| {
-        allocator.free(string);
-        self.string = null;
+        if (!self.comptime_created) {
+            allocator.free(string);
+        }
     }
+    self.string = null;
 }
 
 pub fn length(self: String) usize {
     return self.string.?.len;
 }
 
-pub fn getSlice(self: String) []Char {
+pub fn getSlice(self: String) []const Char {
     return self.string.?;
 }
 
-pub fn getSentinal(self: String) [:0]Char {
+pub fn getSentinal(self: String) [:0]const Char {
     return self.string.?;
 }
 
 pub fn concat(self: String, other: String) String {
-    const new_str = std.mem.concatWithSentinel(allocator, Char, &[_][]const Char{ self.string.?, other.string.? }, '\x00') catch unreachable;
+    if (@inComptime()) {
+        return String{
+            .comptime_created = true,
+            .string = self.string.? ++ other.string.?,
+        };
+    } else {
+        const new_str = std.mem.concatWithSentinel(allocator, Char, &[_][]const Char{ self.string.?, other.string.? }, '\x00') catch unreachable;
 
-    return String{
-        .string = new_str,
-    };
+        return String{
+            .string = new_str,
+        };
+    }
 }
 
 pub fn equal(self: String, other: String) bool {
@@ -65,16 +84,17 @@ pub fn format(
 
 const testing = std.testing;
 
-test "Create string" {
+test "create string" {
     var str = String.create("abc");
-    defer str.destroy();
 
     try testing.expect(str.string != null);
     try testing.expectEqual(3, str.length());
     try testing.expectEqualStrings("abc", str.getSlice());
+    str.destroy();
+    try testing.expectEqual(null, str.string);
 }
 
-test "Concat string" {
+test "concat string" {
     var str1 = String.create("abc");
     defer str1.destroy();
 
@@ -99,4 +119,44 @@ test "sentinals" {
     try testing.expectEqual(3, str.length());
     try testing.expectEqualStrings(src_str, str.getSlice());
     try testing.expectEqualSentinel(Char, '\x00', src_str, str.getSentinal());
+}
+
+test "comptime create" {
+    comptime {
+        var str = String.create("abc");
+
+        try testing.expect(str.string != null);
+        try testing.expectEqual(true, str.comptime_created);
+        try testing.expectEqual(3, str.length());
+        try testing.expectEqualStrings("abc", str.getSlice());
+        str.destroy();
+        try testing.expectEqual(null, str.string);
+    }
+
+    var str = comptime String.create("abc");
+
+    try testing.expect(str.string != null);
+    try testing.expectEqual(true, str.comptime_created);
+    try testing.expectEqual(3, str.length());
+    try testing.expectEqualStrings("abc", str.getSlice());
+    str.destroy();
+    try testing.expectEqual(null, str.string);
+}
+
+test "comptime concat" {
+    comptime {
+        var str1 = String.create("abc");
+        defer str1.destroy();
+
+        var str2 = String.create("def");
+        defer str2.destroy();
+
+        var str_combined = str1.concat(str2);
+        defer str_combined.destroy();
+
+        try testing.expect(str_combined.string != null);
+        try testing.expectEqual(true, str_combined.comptime_created);
+        try testing.expectEqual(6, str_combined.length());
+        try testing.expectEqualStrings("abcdef", str_combined.getSlice());
+    }
 }
